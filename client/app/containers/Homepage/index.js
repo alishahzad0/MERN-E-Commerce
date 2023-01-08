c001eef01c7df9030a721cde249f15fbc026d212
AbdullahG909 <93188887+AbdullahG909@users.noreply.github.com>
2023-01-08 15:33:08 +0000

    MCanges  in homepage

--- client/app/containers/Homepage/index.js
+++ client/app/containers/Homepage/index.js
@@ -7,8 +7,7 @@
 import React from 'react';
 
 import { connect } from 'react-redux';
-import { Row, Col } from 'reactstrap';
-
+import "../../styles/core/_homepage.scss"
 import actions from '../../actions';
 import banners from './banners.json';
 import CarouselSlider from '../../components/Common/CarouselSlider';
@@ -18,36 +17,20 @@ class Homepage extends React.PureComponent {
   render() {
     return (
       <div className='homepage'>
-        <Row className='flex-row'>
-          <Col xs='12' lg='6' className='order-lg-2 mb-3 px-3 px-md-2'>
             <div className='home-carousel'>
               <CarouselSlider
                 swipeable={true}
                 showDots={true}
                 infinite={true}
-                autoPlay={false}
+                autoPlay={true}
                 slides={banners}
-                responsive={responsiveOneItemCarousel}
-              >
+                responsive={responsiveOneItemCarousel}>
                 {banners.map((item, index) => (
                   <img key={index} src={item.imageUrl} />
                 ))}
               </CarouselSlider>
             </div>
-          </Col>
-          <Col xs='12' lg='3' className='order-lg-1 mb-3 px-3 px-md-2'>
-            <div className='d-flex flex-column h-100 justify-content-between'>
-              <img src='/images/banners/banner-2.jpg' className='mb-3' />
-              <img src='/images/banners/banner-5.jpg' />
-            </div>
-          </Col>
-          <Col xs='12' lg='3' className='order-lg-3 mb-3 px-3 px-md-2'>
-            <div className='d-flex flex-column h-100 justify-content-between'>
-              <img src='/images/banners/banner-2.jpg' className='mb-3' />
-              <img src='/images/banners/banner-6.jpg' />
-            </div>
-          </Col>
-        </Row>
+         
       </div>
     );
   }
